3df940fbbe9e295871d5cafc8a10e75c0cd70f5f
Andhitia Rama <andhitia.r@gmail.com>
2019-09-18 17:21:39 +0700

    Add latest tag

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ set -ex
 
 version=$DOCKER_TAG
 
+if [ "$version" == latest ]; then
+    version=oca
+fi
+
 docker image build \
     --file "$version.Dockerfile" \
     --tag "$IMAGE_NAME:$version" \
